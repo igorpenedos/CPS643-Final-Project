bb1f447aa8d504ac1ba263d97c8d21ad2abfaa32
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2023-04-12 02:35:10 -0400

    remove extra import

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class BaseController : MonoBehaviour
 {
